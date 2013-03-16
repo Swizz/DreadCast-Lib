2f57093dd1c1c536b389085cd7c1dfe15bdd5a49
Swizz540 <quentin.g@laposte.net>
2013-03-16 14:46:00 +0100

    -- Application de la LGPL à la version distribuable

--- dist/dreadcastlib.js
+++ dist/dreadcastlib.js
@@ -1,6 +1,11 @@
 /*! DreadCastLib - v0 - 2013-03-16
 * http://www.dreadcast.me/
-* Copyright (c) 2013 Nobody; Licensed WTF Public License */
+* Copyright (c) 2013 Quentin Gerodel (aka Swizz540 | Gideon); Licensed  
+ This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser Public License as published by the Free Software Foundation, either version 3 of the License, or(at your option) any later version.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser Public License for more details.
+
+ You should have received a copy of the GNU Lesser Public License along with this program. If not, see <http://www.gnu.org/licenses/>.*/
 
 /*--------------Init.js-----------------*/
 var VRAI = true; 
