3bb6c873e8633de0afff991d4d6b4a4b02679112
Swizz540 <quentin.g@laposte.net>
2013-03-16 14:54:52 +0100

    - Ajout des contributeurs à la licence

--- dist/dreadcastlib-dev0.js
+++ dist/dreadcastlib-dev0.js
@@ -5,7 +5,9 @@
 
  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser Public License for more details.
 
- You should have received a copy of the GNU Lesser Public License along with this program. If not, see <http://www.gnu.org/licenses/>.*/
+ You should have received a copy of the GNU Lesser Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+* Contributors : nobody, noone */
 
 /*--------------Init.js-----------------*/
 var VRAI = true; 
